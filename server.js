f1acbee856443793276de34d47b4f80cce3c3cac
ahmad Lutfy <ahmadlutfy577@gmail.com>
2025-11-11 20:26:01 +0300

    Refactor game logic and improve player handling
    - Restructured server code with MainEvent handlers
    - Added unique IDs to cards and deep cloning
    - Improved turn management and card effects (+2, +4, stop, direction)
    - Updated client-side UI and interaction logic
    - Fixed bugs in card dealing and play sequence

--- server.js
+++ server.js
@@ -1,190 +1,265 @@
+const fs = require("fs");
+const http  =require('http');
 const Websocket = require('ws');
-const http = require('http');
-const fs = require('fs');
-const { type } = require('os');
 
-const server = http.createServer((req,res) => {
+const server = http.createServer((req ,res) => {
     if (req.url === '/')
     {
         fs.readFile('index.html' , (err,data) => {
+            if (err) console.log(err);
             res.end(data);
         })
     }
-    if(req.url === '/css.css')
+    if (req.url === '/style.css')
     {
-        fs.readFile('css.css' , (err,data) => {
-            res.end(data);
+        fs.readFile('style.css' , (err,data) => {
+            if (err) console.log(err);
+            res.end(data)
         })
     }
-})
+});
+const ws = new Websocket.Server({server:server});
+
+let MainEvent =  {
+    play(data){
+            setpsAfterPlay(data) 
+            if(data.number === 'change dirction')
+            {
+                gameState.state = !gameState.state;
+                playNowFunction();  
+            }
+            if (data.number === '+4')shortWayPlusCard(false ,data);
+            if (data.number === '+2')shortWayPlusCard(true ,data);
+            if (data.number === 'stop') 
+            {
+                if (gameState.state) gameState.indexOfplayer ++;
+                else gameState.indexOfplayer--;
+                playNowFunction();
+            }
+           
+            else if (data.number !=='+4' && data.number !== '+2')playNowFunction();
+    },
+    help(data){
+            if (gameState.PlussCardTotal === 0)
+            {
+                let storgeCardForUser = [...cards(1)];
+                userSend(gameState.indexOfplayer ,  {
+                    type:'cards',
+                    cards:storgeCardForUser
+                })
+                storgeCardForUser.forEach(element => {
+                    players[gameState.indexOfplayer].cards.push(element)
+                })
+                playNowFunction();
+            }
+            else if (gameState.PlussCardTotal > 0)
+            {
+                let storgeCardForUser = [...cards(gameState.PlussCardTotal)];
+                userSend(gameState.indexOfplayer ,  {
+                    type:'cards',
+                    cards:storgeCardForUser
+                })
+                gameState.PlussCardTotal = 0;
+                storgeCardForUser.forEach(element => {
+                    players[gameState.indexOfplayer].cards.push(element)
+                })
+                playNowFunction();
+            }
+    }
+}
+
+let unieqID = 0;
+const clients = new Map();
+let numberOfUser = 0;
+const gameState = {
+    PlussCardTotal:0,
+    indexOfplayer: 0,
+    lastcard : {
+    number:-1,
+    color:-1
+    },
+    state:true,
+}
 
 function cards(number){
-    if (number === '+4')
-    {
-        number = 4;
-    }
-    if (number === '+2')
-    {
-        number = 2;
-    }
-    let b = 'blue';
-    let g = 'green';
-    let y = 'yellow';
-    let r = 'red';
-    let bl = 'black';
-const arr = [{n:1, c:b} , {n:2 , c:b} , {n:3 , c:b} ,  {n:4, c:b} , {n:5 , c:b} , {n:6 , c:b},  {n:7 , c:b} ,{n:8 , c:b} , {n:9 , c:b} , {n:0 , c:b}
-    ,        {n:1, c:g} , {n:2 , c:g} , {n:3 , c:g} ,  {n:4, c:g} , {n:5 , c:g} , {n:6 , c:g},  {n:7 , c:g} ,{n:8 , c:g} , {n:9 , c:g} , {n:0 , c:g}
-    ,        {n:1, c:y} , {n:2 , c:y} , {n:3 , c:y} ,  {n:4, c:y} , {n:5 , c:y} , {n:6 , c:y},  {n:7 , c:y} ,{n:8 , c:y} , {n:9 , c:y} , {n:0 , c:y}
-    ,        {n:1, c:r} , {n:2 , c:r} , {n:3 , c:r} ,  {n:4, c:r} , {n:5 , c:r} , {n:6 , c:r},  {n:7 , c:r} ,{n:8 , c:r} , {n:9 , c:r} , {n:0 , c:r},
-             {n:'+4' , c:bl} , {n:"select color" , c:bl} , {n:'+4' , c:bl} , {n:'+4' , c:bl} , {n:'+4' , c:bl} , {n:'+4' , c:bl} , {n:'+4' , c:bl} , {n:'+4' , c:bl} , {n:'+4' , c:bl} , {n:'+4' , c:bl} , {n:'+4' , c:bl} , {n:'+4' , c:bl} , 
-             {n:'stop' , c:y} , {n:'chenge dirction' , c:y} , {n:'+2' ,  c:y},
-             {n:'stop' , c:b} , {n:'chenge dirction' , c:b} , {n:'+2' ,  c:b},
-             {n:'stop' , c:g} , {n:'chenge dirction' , c:g} , {n:'+2' ,  c:g},
-             {n:'stop' , c:r} , {n:'chenge dirction' , c:r} , {n:'+2' ,  c:r}
-]
+    if (number === '+4')number = 4;
+    
+    if (number === '+2')number = 2;
+    let arr2 = [];
+    const color = ['blue' , 'red' , 'yellow' , 'green'];
+    color.forEach(color => {
+        for (i = 0 ; i < 10 ; i++)
+        {
+            arr2.push({c:color , n:i , Id:0});
+        }
+    })
+    const speicals = ['change dirction', '+2', 'stop']
+    color.forEach(color => {
+        speicals.forEach(speical => {
+            arr2.push({c:color , n:speical , Id:0})
+        });
+    });
+    const balckCard = ['+4' , 'select color'];
+    balckCard.forEach(Card => {
+        arr2.push({c:'black' , n:Card , Id:0});
+    })
 let cardPlay = [];
  for(let i = 0 ; i <number ; i++)
  {
     let min = 0;
-    let max = 64;   
+    let max = arr2.length - 1;   
     let random = Math.floor(Math.random() *(max - min + 1)) + min;
-    cardPlay.push(arr[random]);
+    let FirstDeepCopyCard = structuredClone(arr2[random]);
+    if (cardPlay.includes(FirstDeepCopyCard))
+    {
+        let NextDeepCopyCard = structuredClone(FirstDeepCopyCard);
+        cardPlay.push(NextDeepCopyCard);
+    }
+    else cardPlay.push(FirstDeepCopyCard);
+    
  }
- console.log(arr.length)
+ for (let i= 0 ; i < cardPlay.length ; i++)
+ {
+    cardPlay[i].Id = unieqID++
+ }
+
  return cardPlay;
 }
 
-let cardPlus = {
-    color: -1,
-    number:-1
+function nextPlyer(state){
+    if (gameState.indexOfplayer === players.length ) {gameState.indexOfplayer = 0;}
+    else if (gameState.indexOfplayer === -1) {gameState.indexOfplayer = 1;}
+    if (state) {gameState.indexOfplayer++}
+    else {gameState.indexOfplayer--};
+    if (gameState.indexOfplayer === players.length ) {gameState.indexOfplayer = 0;}
+    else if (gameState.indexOfplayer === -1) {gameState.indexOfplayer = 1;}
 }
-const clients = new Map();
-
-const ws =new Websocket.Server({server:server})
-
-let contOFplay = 0;
-
-ws.on('connection' , (socket) => {
-    contOFplay++
-
-        clients.set(`ID${contOFplay}` ,socket);
-
-        function userSend(number , data){
-            if (number === 2){number = 0};
-            const user = ['ID1' , 'ID2'];
-            const player = clients.get(user[number])
-            if (player && player.readyState === Websocket.OPEN)
-            {
-                player.send(JSON.stringify(data))
-            }
-        }
-
-        function sendTOall(data){
-            ws.clients.forEach(element => {
-                if (element &&element.readyState === Websocket.OPEN)
-                {
-                    element.send(JSON.stringify(data))
-                }                
-            });
-        }
-        if (contOFplay ===2)
+function checkCard (){
+    let arrOfindexs = [];
+    // console.log(players[gameState.indexOfplayer].cards)
+    players[gameState.indexOfplayer].cards.forEach(card => {
+        if (   card.n === gameState.lastcard.number 
+            || card.c === gameState.lastcard.color
+            || card.c === 'black'|| gameState.lastcard.number === -1 )
         {
-            userSend(0 ,{
+            arrOfindexs.push(players[gameState.indexOfplayer].cards.indexOf(card));
+        }
+    });
+    return arrOfindexs;
+}
+function deletCardFromUser(data){
+    players[gameState.indexOfplayer].cards.splice(data.indexOfCard , 1)
+}
+
+function userSend(index , data){
+    if (players[index].socket.readyState === Websocket.OPEN)
+    {
+        players[index].socket.send(JSON.stringify(data))
+    }
+}
+
+function checkPlusCard(x){
+    let arrOfindexs = [];
+    players[gameState.indexOfplayer].cards.forEach(card => {
+        if (('+2' === card.n &&(gameState.lastcard.color === card.c || x))|| card.n === '+4')
+            {
+                arrOfindexs.push(players[gameState.indexOfplayer].cards.indexOf(card))//is it important?
+            }
+        })
+        return arrOfindexs;
+    
+}
+let players = [
+    {ID:'ID0' , socket:null , cards:[]},
+    {ID:'ID1' , socket:null , cards:[]}
+
+]
+function playNowFunction(){
+    nextPlyer(gameState.state);
+    userSend(gameState.indexOfplayer , {
+        type:'playNow',
+        index:checkCard(),
+    });
+}
+function shortWayPlusCard(x ,data)
+{
+    let value = data.number.split('')
+    gameState.PlussCardTotal = gameState.PlussCardTotal + Number(value[1]);
+    nextPlyer(gameState.state)
+    userSend(gameState.indexOfplayer , {
+        type:'playNow',
+        index:checkPlusCard(x)
+    })
+}
+function sendTOall(data){
+    ws.clients.forEach(element => {
+        if (element &&element.readyState === Websocket.OPEN)
+        {
+            element.send(JSON.stringify(data))
+        }                
+    });
+}
+
+function setpsAfterPlay(data){
+    console.log(players[gameState.indexOfplayer].cards.length)
+    if (players[gameState.indexOfplayer].cards.length === 1)
+    {
+        console.log("someone  end the game");
+        userSend(gameState.indexOfplayer , {
+            type:'winner',
+        })
+        nextPlyer();
+        userSend(gameState.indexOfplayer , {
+            type:'loser',
+        })
+    }
+    else {
+    gameState.lastcard.number = data.number;
+    gameState.lastcard.color =data.color;
+    deletCardFromUser(data);
+    sendTOall({
+        type:'lastcard',
+        card:gameState.lastcard
+    })
+    }
+}
+ws.on('connection' , socket => {
+    players[numberOfUser].socket = socket;
+    numberOfUser++;
+    if (numberOfUser === 2)
+    {
+
+        for (let i = 0 ; i < players.length ; i++)
+        {
+            userSend(i , {
                 type:'order',
-                order:0
+                order:i
             })
-            userSend(1 , {
-                type:'order',
-                order:1
-            })
-            /*send to all doesnt work */
-            userSend(0 , {
+            let copycard = [...cards(10)];
+            userSend(i , {
                 type:'cards',
-                cards:cards(10)
+                cards:copycard
             })
-            userSend(1 , {
-                type:'cards',
-                cards:cards(10)
-            })
-            userSend(0 , {
-                type:'start',
-            })
-        }
 
-    socket.on('message' , (event) => {
+            players[i].cards = [...copycard];
+            nextPlyer();
+        }
+        playNowFunction();
+    }
 
-        const data = JSON.parse(event);
-        let cardInfo  = {
-            type:'lastcard',
-            color:data.color,
-            number:data.number,
-            playerNumber: data.order
-        }
-        if (data.type === 'play a card')
+    socket.on('message' , event => {
+        let data;
+        try {
+             data = JSON.parse(event);
+        } catch(error)
         {
-            if (data.number === 'select color')
-            {
-                sendTOall(cardInfo);
-                userSend(data.order + 1 , {type:'start'});
-            }
-            else if (data.number === 'stop' || data.number === 'chenge dirction')
-            {
-                sendTOall(cardInfo);
-                userSend(data.order , {type:'start'});
-            }
-            else if (data.number === '+4')
-            {
-                let x = data.number.split('');
-                cardPlus.number = Number(x[1]);
-                sendTOall(cardInfo)
-                userSend(data.order +1 , {type:"ready-or-not"});
-            }
-            else if (data.number === '+2')
-            {
-                let x = data.number.split('');
-                cardPlus.number = Number(x[1]);
-                sendTOall({
-                    type:'lastcard',
-                    color:data.color,
-                    number:data.number,
-                    playerNumber: data.order
-                })
-                userSend(data.order +1 , {type:"+2"});
-            }
-            else 
-            {
-                sendTOall(cardInfo);
-                userSend(data.order + 1, {type: 'start'})
-            }
+            console.error(error)
         }
-        if (data.type === 'help')
-        {
-            if (data.state)
-            {
-                userSend(data.order , {
-                    type:'cards',
-                    cards:cards(1)
-                })
-                userSend(data.order + 1, {type:'start'})
-            }
-            else {
-                userSend(data.order , {
-                    type:'cards',
-                    cards:cards(cardPlus.number),
-                })
-                cardPlus.number = 0;
-                userSend(data.order + 1 , {type:'start'});
-            }
-        }
-        if (data.type === 'yes')
-        {
-            let x = data.number.split('');
-            cardPlus.number += Number(x[1]);
-            sendTOall(cardInfo)
-            userSend(data.order + 1, {type:'ready-or-not'})
-        }
+        const stateOfPlay = MainEvent[data.type];
+        if (stateOfPlay)stateOfPlay(data);
     })
 })
-server.listen(3000 , ()=> {
-    console.log('server is running')
-})
+
+server.listen(3000 , () => {
+    console.log('server is running');
+});
